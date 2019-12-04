a0e85c77dfedc29c1b0d3910bfc451c83e66a8bc
NikolaStojicic <nikolastojicicsd@gmail.com>
2019-12-04 11:44:01 +0100

    Hello

---
+++ src/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-list-group>
+      <b-list-group-item
+        v-for="(item, id) in items"
+        :key="id"
+        class="d-flex justify-content-between align-items-center"
+        variant="primary"
+      >
+        item
+        <b-badge href="#" variant="danger" pill>X</b-badge>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+  data() {
+    return {
+      items: [
+        {
+          variant: "primary",
+          content: "Primary list group"
+        },
+        {
+          variant: "primary",
+          content: "Primary list group"
+        },
+        {
+          variant: "primary",
+          content: "Primary list group"
+        },
+        {
+          variant: "primary",
+          content: "Primary list group"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
